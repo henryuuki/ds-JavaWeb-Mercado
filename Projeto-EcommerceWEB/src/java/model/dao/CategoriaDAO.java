015e195f12e8da00601d8a9150aceecb0aed8bc5
henryuuki <henryuukiaraki@gmail.com>
2024-04-09 15:31:21 -0300

    models pronto

---
+++ Projeto-EcommerceWEB/src/java/model/dao/CategoriaDAO.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.bean.TableCategoria;
+
+
+/**
+ *
+ * @author Senai
+ */
+public class CategoriaDAO {
+    public List<TableCategoria> read() {
+        List<TableCategoria> categorias = new ArrayList();
+
+        try {
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+
+            stmt = conexao.prepareStatement("SELECT * FROM endereço");
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                TableCategoria c = new TableCategoria();
+                c.setId_categoria(rs.getInt("id_categoria"));
+                c.setNome(rs.getString("nome"));
+                categorias.add(c);
+            }
+
+            rs.close();
+            stmt.close();
+            conexao.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return categorias;
+    }
+    
+    public boolean create(TableCategoria c) {
+        try {
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt = null;
+
+            stmt = conexao.prepareStatement("INSERT INTO categoria (nome) VALUES (?)");
+            stmt.setString(1, c.getNome());
+          
+            stmt.executeUpdate();
+
+            stmt.close();
+            conexao.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }      
+    }
+    
+    public void update(TableCategoria c) {
+        
+        try {
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt;
+            
+            stmt = conexao.prepareStatement("UPDATE endereço SET nome = ? WHERE id_categoria = ?");
+            stmt.setString(1, c.getNome());
+            
+            stmt.executeUpdate();
+            
+            stmt.close();
+            conexao.close();            
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void delete(TableCategoria en){
+        try{
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt;
+            
+            stmt = conexao.prepareStatement("DELETE FROM categoria WHERE id_categoria = ?");
+            stmt.setInt(1, en.getId_categoria());
+            
+            stmt.executeUpdate();
+            
+            stmt.close();
+            conexao.close();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
